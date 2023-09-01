8f1dbd85946e407f6244482dc0440bd125d64de6
sfjbs <pooja@email.com>
2023-09-01 06:50:48 +0000

    First maven project

---
+++ day01/simple-maven-app/src/main/java/com/pooja/data/App.java
@@ -0,0 +1,13 @@
+package com.pooja.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Pooja Here" );
+    }
+}
